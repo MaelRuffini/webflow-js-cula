2a7a87191068a1d59b4b349d8c0259f2cc8f4921
MaelRuffini <mael.lucien.ruffini@gmail.com>
2023-11-21 20:10:12 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -8,6 +8,32 @@ import homePannel from './pages/home/homePannel'
 import header from './components/header'
 import webgl from './components/webgl'
 
+function refreshPage() {
+  window.location.reload()
+}
+
+const mediaQuery768 = window.matchMedia('(max-width: 768px)')
+const mediaQuery991 = window.matchMedia('(max-width: 991px)')
+
+function setupBreakpointListeners() {
+  mediaQuery768.addEventListener((e) => {
+    if (e.matches) {
+      console.log('Viewport is now under 768px, refreshing page...')
+      refreshPage()
+    }
+  })
+
+  mediaQuery991.addEventListener((e) => {
+    if (e.matches) {
+      console.log('Viewport is now under 991px, refreshing page...')
+      refreshPage()
+    }
+  })
+}
+
+// Initial setup
+setupBreakpointListeners()
+
 if (window.matchMedia('(max-width: 991px)').matches) {
   const lenis = new Lenis({
     smoothTouch: true,
