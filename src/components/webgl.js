2b78f07cd1964f10026324f72bfb2ad03fee815f
MaelRuffini <mael.lucien.ruffini@gmail.com>
2023-11-29 18:52:20 +0700

    Update webgl.js

--- src/components/webgl.js
+++ src/components/webgl.js
@@ -245,7 +245,7 @@ export default function webgl() {
       bakedFactories.flipY = false
       bakedFactories.SRGBColorSpace = THREE.SRGBColorSpace
 
-      const bakedTerrain = textureLoader.load('https://uploads-ssl.webflow.com/651309ab2c6e146a99437841/656708c8dc2e3d05781bf4b3_terrain.jpg')
+      const bakedTerrain = textureLoader.load('https://uploads-ssl.webflow.com/651309ab2c6e146a99437841/656725d53202c6e50ad6dfdf_terrain.jpg')
       bakedTerrain.flipY = false
       bakedTerrain.SRGBColorSpace = THREE.SRGBColorSpace
 
